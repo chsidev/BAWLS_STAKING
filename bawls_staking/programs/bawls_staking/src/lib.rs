322724f65d0ee97ad6bc0c59dd2f3c2b41442d0e
chsidev <127224337+chsidev@users.noreply.github.com>
2025-06-13 22:15:39 +0700

    create

--- bawls_staking/programs/bawls_staking/src/lib.rs
+++ bawls_staking/programs/bawls_staking/src/lib.rs
@@ -1,29 +1,13 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::declare_id;
 
-declare_id!("8eP5KqibfaYq7ANUjEpy6t1Jdq9i3kjwohCDEmCNt3nv");
+declare_id!("9426uyFBpzXhVRxmfXzFoCgWP9shVJpYevsbSKoDyKkn");
 
 #[program]
 pub mod bawls_staking {
     use super::*;
 
-    pub fn stake(ctx: Context<Stake>, amount: u64) -> Result<()> {
-        // Transfer tokens from user to vault
-        let cpi_accounts = Transfer {
-            from: ctx.accounts.user_token_account.to_account_info(),
-            to: ctx.accounts.vault_token_account.to_account_info(),
-            authority: ctx.accounts.user.to_account_info(),
-        };
-
-        let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
-        token::transfer(cpi_ctx, amount)?;
-
-        // Save stake data
-        let stake_data = &mut ctx.accounts.stake_state;
-        stake_data.amount = amount;
-        stake_data.start_time = Clock::get()?.unix_timestamp;
-        stake_data.user = ctx.accounts.user.key();
-
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 }
